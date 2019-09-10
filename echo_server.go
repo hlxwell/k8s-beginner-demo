579187acac30c95d0ccae3dfabc76b9ee9a968da
Michael He <hlxwell@gmail.com>
2019-09-10 18:18:34 +0900

    add hostname into the output

--- echo_server.go
+++ echo_server.go
@@ -15,7 +15,8 @@ func HomeHandler(c echo.Context) error {
 	for i := 0; i < 1000; i++ {
 		total += rand.Intn(1000)
 	}
-	log.Println("/ been visited at:", time.Now(), "-", total)
+
+	log.Println("/ been visited at:", time.Now(), "-", total, "-", c.Request().Host)
 	return c.String(http.StatusOK, "Hello, World! version 1")
 }
 
